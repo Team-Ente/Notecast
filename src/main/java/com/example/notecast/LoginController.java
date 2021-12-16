2ab45cb0b2f2143439c69e5dbacf7f93bacd4de8
Tariquzzaman-faisal <tariquzzaman@iut-dhaka.edu>
2021-12-16 22:36:13 +0600

    Login UI Created

---
+++ src/main/java/com/example/notecast/LoginController.java
@@ -0,0 +1,17 @@
+package com.example.notecast;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class LoginController {
+    @FXML
+    private ImageView notecastImageview;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button cancelButton;
+
+}
