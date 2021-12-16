8c2ae5edcbf26f0a32baf340f3008e3970979f97
Tariquzzaman-faisal <tariquzzaman@iut-dhaka.edu>
2021-12-16 23:36:55 +0600

    Login UI Updated

--- src/main/java/com/example/notecast/HelloApplication.java
+++ src/main/java/com/example/notecast/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -12,6 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setTitle("Notecast");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/notecast/LoginController.java
+++ src/main/java/com/example/notecast/LoginController.java
@@ -1,10 +1,14 @@
 package com.example.notecast;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class LoginController {
     @FXML
@@ -13,5 +17,23 @@ public class LoginController {
     private Button loginButton;
     @FXML
     private Button cancelButton;
+    @FXML
+    private Label Error;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField userpasswordField;
 
+    public void cancelbuttonAction(ActionEvent e)
+    {
+        Stage stage = (Stage)cancelButton.getScene().getWindow();
+        stage.close();
+    }
+    public void loginButtonAction(ActionEvent e)
+    {
+        if(usernameTextField.getText().isBlank() || usernameTextField.getText().isBlank())
+            Error.setText("Invalid Username or Password");
+        else
+            Error.setText("Success");
+    }
 }
